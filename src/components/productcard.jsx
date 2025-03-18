bdd3f7294bfa9a715c642bf91bc5fd22b9df4692
siva kumar kalagandha <bhanusree414@gmail.com>
2025-03-18 13:52:26 +0530

    added

--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,22 @@
 // write product card here
+// write product card here
+import React from 'react';
+import ViewProductButton from '../components/button';
+
+
+const ProductCard = () => {
+  const productImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLPIvQRxNly8mJ8eTU4B2d3yH3RTlLmt84Jg&s';
+  const productName = 'light stick';
+  const productPrice = '₹5000';
+
+  return (
+    <div style={{ border: '1px solid #ccc', padding: '16px', textAlign: 'center', borderRadius: '8px', width: '250px' }}>
+      <img src={productImage} alt="Product" style={{ width: '100%', height: '250px', objectFit: 'cover', borderRadius: '8px' }} />
+      <h3>{productName}</h3>
+      <p style={{ color: 'green', fontWeight: 'bold' }}>{productPrice}</p>
+      <ViewProductButton />
+    </div>
+  );
+};
+
+export default ProductCard;
